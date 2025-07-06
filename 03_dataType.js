2f866183a034ef8877ee92faf76d68b819ac1cc5
SHUBBHAM-KUMARR <shubbhammkumarr@outlook.com>
2025-07-06 15:29:07 +0530

    updated

---
+++ 03_dataType.js
@@ -0,0 +1,16 @@
+// let check each data type ,we are not going to chane our values so we will be using const to store our values
+const username = "shubham_kr";
+const id = 123456;
+const cgpa = 8.7;
+const passed = true;
+let placed; //we used let as const need value immediately it get declared
+const status = null;
+const sym = Symbol("user");
+
+console.log(typeof(username));
+console.log(typeof(id));
+console.log(typeof(cgpa));
+console.log(typeof(passed));
+console.log(typeof(placed));
+console.log(typeof(status));
+console.log(typeof(sym));
