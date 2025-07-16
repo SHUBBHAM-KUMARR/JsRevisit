f8aafde60dc738cfdda0748350de5e887e330850
SHUBBHAM-KUMARR <shubbhammkumarr@outlook.com>
2025-07-16 14:29:38 +0530

    dom manipulation

--- DOM/dom_codes/01_intro.js
+++ DOM/dom_codes/01_intro.js
@@ -1,1 +1,13 @@
+// we cannot learn dom without visualising the nodes and childrens 
 
+// this code file links the index.html file of this directory
+
+const  firstDomLearning = document.getElementsByClassName('first')[0]//here we are calling  the html document's div whose class is first
+
+
+firstDomLearning.style.width = "10vw";
+firstDomLearning.style.height = "80px"
+firstDomLearning.style.background = " blue"
+firstDomLearning.style.color = " white"
+
+// 
