fe7d3d6a8c07fe54c17779cdb036904ab89f137b
SHUBBHAM-KUMARR <shubbhammkumarr@outlook.com>
2025-07-16 15:49:21 +0530

    dom

--- DOM/dom_codes/01_intro.js
+++ DOM/dom_codes/01_intro.js
@@ -2,12 +2,18 @@
 
 // this code file links the index.html file of this directory
 
-const  firstDomLearning = document.getElementsByClassName('first')[0]//here we are calling  the html document's div whose class is first
+const rootElement = document.getElementById('html')
+rootElement.style.background="black"
+rootElement.style.padding="0px"
+rootElement.style.margin="0px"
 
+const  firstDomLearning = document.getElementById('header')//here we are calling  the html document's div whose class is first
 
-firstDomLearning.style.width = "10vw";
+firstDomLearning.style.width = "100vw";
 firstDomLearning.style.height = "80px"
-firstDomLearning.style.background = " blue"
+firstDomLearning.style.background = " slategray"
 firstDomLearning.style.color = " white"
+firstDomLearning.style.padding="12px"
+// so in similar way we can maipulate other nodelist and properties
 
-// 
+// not practisiable in real world as it leads inline styling of elements
