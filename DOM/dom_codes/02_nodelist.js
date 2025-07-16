66bb227e7a830470c572f5c80dba024362b426fa
SHUBBHAM-KUMARR <shubbhammkumarr@outlook.com>
2025-07-16 18:21:18 +0530

    nodelist

--- DOM/dom_codes/02_nodelist.js
+++ DOM/dom_codes/02_nodelist.js
@@ -1,6 +1,12 @@
 const cont = document.querySelector('span')
 cont.addEventListener('click',()=>{
-  cont.style.display = "none"
+  cont.style.display = "none";
   
 })// here selcted  span and set its display property to none
 
+// query selector all 
+const allH1 = document.querySelectorAll(".testH1")
+allH1[0].style.color = "red"
+
+const newALlh1 = Array.from(allH1)
+newALlh1.forEach((val)=>(val.style.color = "yellow"))//color changed to yellow
