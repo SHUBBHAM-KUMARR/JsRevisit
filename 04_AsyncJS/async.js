2542b68d3012fbb6bc0ca8a31bf36d19e47c86bc
SHUBBHAM-KUMARR <shubbhammkumarr@outlook.com>
2025-07-19 00:47:01 +0530

    updated files

---
+++ 04_AsyncJS/async.js
@@ -0,0 +1,31 @@
+// console.log('shubham');
+// console.log('shubham');
+// setInterval(()=>{
+//   console.log("shubham , from set interval");
+  
+// },2000)
+const hexColorGenrator = function() {
+  const range = '0123456789ABCDEF'; 
+  let color = '#';
+  for(let i = 0; i < 6; i++) {
+    color += range[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+
+const btnStr = document.getElementById('start');
+const btnStp = document.getElementById('stop');
+const textAr = document.getElementById('text');
+ let interval;
+btnStr.addEventListener('click', function() {
+  // generate hex code every second
+   interval = setInterval(() => {
+    textAr.innerText = hexColorGenrator();
+  }, 1000);
+});
+
+btnStp.addEventListener('click',()=>{
+  clearInterval(interval)
+
+}
+)
